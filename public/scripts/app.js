63e76b6f4ad10806e1788a579ff29dc2a9235faa
ritabradley <contact@ritabradley.com>
2019-12-11 14:04:06 -0800

    Setup compiler
    - added Babel
    - added scripts/app.js
    - added src/app.js

---
+++ public/scripts/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var template = React.createElement(
+  "p",
+  null,
+  "JSX paragraph"
+);
+var root = document.getElementById("root");
+
+ReactDOM.render(template, root);
---
+++ src/app.js
@@ -0,0 +1,4 @@
+const template = <p>JSX paragraph</p>;
+const root = document.getElementById("root");
+
+ReactDOM.render(template, root);

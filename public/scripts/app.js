b5b759cf9ff8de6eab53b010ad17e6ba4666e607
Rita Bradley <contact@ritabradley.com>
2019-12-11 19:40:54 -0800

    JSX - Dynamic info
    - use JSX expressions to render dynamic content

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,53 @@
-"use strict";
+'use strict';
+
+var siteInfo = {
+  title: 'Indecision App',
+  subtitle: 'Let me do the thinking for you!'
+};
 
 var template = React.createElement(
-  "p",
+  'div',
   null,
-  "JSX paragraph"
+  React.createElement(
+    'h1',
+    null,
+    siteInfo.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    siteInfo.subtitle
+  )
 );
-var root = document.getElementById("root");
+
+var user = {
+  name: 'Rita Bradley',
+  age: 32,
+  location: 'Las Vegas, NV'
+};
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: ',
+    user.location
+  )
+);
+
+var root = document.getElementById('root');
 
 ReactDOM.render(template, root);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,29 @@
-const template = <p>JSX paragraph</p>;
-const root = document.getElementById("root");
+let siteInfo = {
+  title: 'Indecision App',
+  subtitle: 'Let me do the thinking for you!'
+};
+
+const template = (
+  <div>
+    <h1>{siteInfo.title}</h1>
+    <p>{siteInfo.subtitle}</p>
+  </div>
+);
+
+let user = {
+  name: 'Rita Bradley',
+  age: 32,
+  location: 'Las Vegas, NV'
+};
+
+const templateTwo = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+);
+
+const root = document.getElementById('root');
 
 ReactDOM.render(template, root);
